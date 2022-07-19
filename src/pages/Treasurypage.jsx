b43dacb5ec7e353e418589453ccc1ef8c7e95fb1
Pradhan <chrisonpradhan@gmail.com>
2022-07-19 18:05:08 +0530

    fix prettier bug

--- src/pages/Treasurypage.jsx
+++ src/pages/Treasurypage.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import Treasury from '../components/Treasury';
-import { Navbar } from '../components/Navbar';
+import React from "react";
+import Treasury from "../components/Treasury";
+import { Navbar } from "../components/Navbar";
+import { Sidebar } from "../components/Sidebar";
 
 const Treasurypage = () => {
   return (
     <div>
-        <Navbar/>
-        <Treasury/>
+      <Navbar />
+      <div className="flex flex-col md:flex-row justify-center ">
+        <div>
+          <Sidebar />
+        </div>
+        <Treasury />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Treasurypage
+export default Treasurypage;
